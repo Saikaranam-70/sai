57843089ac7e82cebc56d9173ca62021af0ef309
Saikaranam-70 <saikaranam46@gmail.com>
2025-05-20 14:57:30 +0530

    Fix typo in Components path

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react'
 
 import { Routes, Route } from 'react-router-dom'
-import Home from './Componenets/Home/Home'
-import Footer from './Componenets/Footer/Footer'
-import About from './Componenets/About/About'
-import Projects from './Componenets/Project/Projects'
-import ResumeNew from './Componenets/Resume/ResumeNew'
-import Contact from './Componenets/Contact/ContactForm'
+import Home from './Components/Home/Home'
+import Footer from './Components/Footer/Footer'
+import About from './Components/About/About'
+import Projects from './Components/Project/Projects'
+import ResumeNew from './Components/Resume/ResumeNew'
+import Contact from './Components/Contact/ContactForm'
 import Loader from './Loader'
 
-import Navbar from './Componenets/Navbar/Navbar'
+import Navbar from './Components/Navbar/Navbar'
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
